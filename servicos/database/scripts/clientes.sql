5f0cc1b2fd40746fc8c6ef608e7d99b83c7675a1
Alyasaf Meireles <alymeireles62@gmail.com>
2026-08-25 22:11:26 -0300

    feat: ambiente local para o minio e algumas correções

--- servicos/database/scripts/clientes.sql
+++ servicos/database/scripts/clientes.sql
@@ -1,11 +1,11 @@
 CREATE TABLE tb_clientes
 (
-    codigo     SERIAL       NOT NULL PRIMARY KEY,
+    codigo     BIGSERIAL    NOT NULL PRIMARY KEY,
     nome       VARCHAR(150) NOT NULL,
     cpf        VARCHAR(11)  NOT NULL,
     logradouro VARCHAR(100),
     numero     VARCHAR(10),
     bairro     VARCHAR(100),
-    email      VARCHAR(150),
+    email      VARCHAR(150) NOT NULL UNIQUE,
     telefone   VARCHAR(20)
 );
--- servicos/database/scripts/pedidos.sql
+++ servicos/database/scripts/pedidos.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tb_pedidos
 (
-    codigo          SERIAL    NOT NULL PRIMARY KEY,
+    codigo          BIGSERIAL NOT NULL PRIMARY KEY,
     codigo_cliente  BIGINT    NOT NULL,
     data_pedido     TIMESTAMP NOT NULL DEFAULT NOW(),
     chave_pagamento TEXT,
     observacoes     TEXT,
     status          VARCHAR(20) CHECK ( status in ('REALIZADO', 'PAGO', 'FATURADO', 'ENVIADO', 'ERRO_PAGAMENTO',
-                                                   'PREPARANDO ENVIO') ),
+                                                   'PREPARANDO_ENVIO') ),
     total           DECIMAL(16, 2),
     codigo_rastreio VARCHAR(255),
     url_nf          TEXT
@@ -14,7 +14,7 @@ CREATE TABLE tb_pedidos
 
 CREATE TABLE tb_item_pedido
 (
-    codigo         SERIAL         NOT NULL PRIMARY KEY,
+    codigo         BIGSERIAL      NOT NULL PRIMARY KEY,
     codigo_pedido  BIGINT         NOT NULL REFERENCES tb_pedidos (codigo),
     codigo_produto BIGINT         NOT NULL,
     quantidade     INT            NOT NULL,
